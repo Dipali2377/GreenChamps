4e269821a2178ddab4f0ef310a2c60656a56c177
Dipali2377 <dipalim680@gmail.com>
2025-04-11 15:24:55 +0530

    added mongo atlas connection string

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async (req, res) => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URL || "mongodb://127.0.0.1:27017/greenchampsDB"
+      process.env.MONGO_URL ||
+        "mongodb+srv://dipalim680:Greenchamps123@cluster0.1bt3psm.mongodb.net/"
     );
     console.log(`Connected to DB successfully...!`);
   } catch (error) {
